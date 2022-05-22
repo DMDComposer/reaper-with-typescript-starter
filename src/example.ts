bd9510e8e42cdb1da1ede3d56f444f95f44dfed1
Dillon M. DeRosa <53786232+DMDComposer@users.noreply.github.com>
2022-05-21 21:42:06 -0400

    inti

--- src/example.ts
+++ src/example.ts
@@ -1,22 +1,26 @@
-const project = reaper.GetCurrentProjectInLoadSave()
-const numTracks = reaper.CountTracks(project)
+// const project = reaper.GetCurrentProjectInLoadSave()
+// const numTracks = reaper.CountTracks(project)
 
-const tracks = [] as MediaTrack[]
+// const tracks = [] as MediaTrack[]
 
-for (let idx = 0; idx < numTracks; idx++)
-  tracks.push(reaper.GetTrack(project, idx))
+// for (let idx = 0; idx < numTracks; idx++) tracks.push(reaper.GetTrack(project, idx))
 
-for (const track of tracks) {
-  const [retval, trackname] = reaper.GetTrackName(track)
-  reaper.ShowConsoleMsg(`Trackname: ${trackname}\n`)
-  reaper.ShowConsoleMsg(`NEW LINE, HOT RELOAD\n`)
-}
+// for (const track of tracks) {
+//   const [retval, trackname] = reaper.GetTrackName(track)
+//   reaper.ShowConsoleMsg(`Trackname: ${trackname}\n`)
+//   reaper.ShowConsoleMsg(`NEW LINE, HOT RELOAD\n`)
+//   reaper.ShowConsoleMsg("testing DMD")
+// }
 
-function main() {
-  const char = gfx.getchar(0)
-  if (char != 27 && char != -1) reaper.defer(main)
-  gfx.update()
-}
+// function main() {
+//   const char = gfx.getchar(0)
+//   if (char != 27 && char != -1) reaper.defer(main)
+//   gfx.update()
+// }
 
-gfx.init("My Window", 640, 480, 0, 200)
-main()
+// gfx.init("My Window", 640, 480, 0, 200)
+// main()
+
+const test = "hello world"
+
+reaper.ShowConsoleMsg(test)
